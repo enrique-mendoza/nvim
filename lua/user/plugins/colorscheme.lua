a00a13afed5463c8cfb338aa717971a91f72bac8
Enrique Mendoza <luis.mendoza@infotec.mx>
2025-02-25 15:13:08 -0600

    update(themes): cyberdream

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -1,11 +1,9 @@
-local M = require "user.themes.eldritch"
+local M = require "user.themes.cyberdream"
 
 function M.config()
-  require("eldritch").setup {
-    palette = "darker"
-  }
+  require("cyberdream").setup {}
 
-  vim.cmd.colorscheme "eldritch"
+  vim.cmd.colorscheme "cyberdream"
 end
 
 return M
