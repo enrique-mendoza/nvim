cdc597b897fa33100431771c906b0a74cbbe7d69
Enrique Mendoza <luis.mendoza@infotec.mx>
2025-04-07 12:30:18 -0600

    feat(theme): update by daytime

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -1,7 +1,19 @@
-local M = require("user.themes.eldritch")
+local M = require "user.themes.eldritch"
+
+-- local function syncOs()
+--   local hr = tonumber(os.date("%H", os.time()))
+--
+--   if hr > 6 and hr < 21 then -- day between 6am and 9pm
+--     return "day"
+--   end
+--
+--   vim.notify "󱫍 Time to update your theme!"
+--
+--   return "night"
+-- end
 
 function M.config()
-  require("eldritch").setup({})
+  require("eldritch").setup {}
 
   vim.cmd.colorscheme "eldritch"
 end
