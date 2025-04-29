192d8835ca0a6c51e5fe9857633e3947f0f67c35
Enrique Mendoza <luis.e.1998@hotmail.com>
2025-04-29 14:43:27 -0600

    feat: kulala

---
+++ lua/user/plugins/kulala.lua
@@ -0,0 +1,19 @@
+local M = {
+  "mistweaverco/kulala.nvim",
+  ft = { "http", "rest" },
+  opts = {
+    -- your configuration comes here
+    global_keymaps = false,
+  },
+}
+
+function M.config()
+  require("kulala").setup {
+    global_keymaps = false,
+    -- dev, test, prod, can be anything
+    -- see: https://learn.microsoft.com/en-us/aspnet/core/test/http-files?view=aspnetcore-8.0#environment-files
+    default_env = "dev",
+  }
+end
+
+return M
