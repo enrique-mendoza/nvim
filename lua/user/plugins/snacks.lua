56f640965ae0fa490d0b37bdadf09e4cdf36e041
Enrique Mendoza <luis.e.1998@hotmail.com>
2025-04-30 20:12:20 -0600

    update(snacks): statuscolumn

--- lua/user/plugins/snacks.lua
+++ lua/user/plugins/snacks.lua
@@ -22,7 +22,7 @@ function M.config()
     quickfile = { enabled = true },
     scope = { enabled = true },
     scroll = { enabled = true },
-    statuscolumn = { enabled = true },
+    -- statuscolumn = { enabled = true },
     words = { enabled = true },
     styles = {
       notification = {
