25797ddd12b5ad5c62aa273d8697d9709943b9f5
Enrique Mendoza <luis.e.1998@hotmail.com>
2025-05-02 19:04:36 -0600

    update(cmp): borders

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -202,7 +202,7 @@ function M.config()
     window = {
       completion = {
         border = "rounded",
-        winhighlight = "Normal:CmpPmenu,CursorLine:PmenuSel,FloatBorder:FloatBorder,Search:None",
+        winhighlight = "Normal:Pmenu,CursorLine:PmenuSel,FloatBorder:FloatBorder,Search:None",
         col_offset = -3,
         side_padding = 1,
         scrollbar = false,
@@ -210,7 +210,7 @@ function M.config()
       },
       documentation = {
         border = "rounded",
-        winhighlight = "Normal:CmpDoc,FloatBorder:FloatBorder,Search:None",
+        winhighlight = "Normal:Pmenu,FloatBorder:FloatBorder,Search:None",
       },
     },
     experimental = {
