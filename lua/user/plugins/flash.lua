3b86fdf12cff44da907ea71a1fdd2387707541f3
Enrique Mendoza <luis.e.1998@hotmail.com>
2025-05-09 19:15:18 -0700

    update(flash): eyeliner compatibility

--- lua/user/plugins/flash.lua
+++ lua/user/plugins/flash.lua
@@ -4,7 +4,35 @@ local M = {
 }
 
 function M.config()
-  require("flash").setup {}
+  require("flash").setup {
+    jump = { nohlsearch = true },
+    prompt = {
+      win_config = {
+        border = "none",
+        -- row = -3, -- Place the prompt above the statusline.
+      },
+    },
+    search = {
+      exclude = {
+        "flash_prompt",
+        "qf",
+        function(win)
+          -- Floating windows from bqf.
+          if vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(win)):match "BqfPreview" then
+            return true
+          end
+
+          -- Non-focusable windows.
+          return not vim.api.nvim_win_get_config(win).focusable
+        end,
+      },
+    },
+    modes = {
+      char = {
+        keys = { "f", "F", "t", "T" },
+      },
+    },
+  }
 end
 
 return M
