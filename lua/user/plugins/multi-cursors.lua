1081f16226deca5a0b4c8c5264fc6b090bfe973d
Enrique Mendoza <luis.e.1998@hotmail.com>
2025-05-09 19:17:13 -0700

    update(multi-cursors): colors

--- lua/user/plugins/multi-cursors.lua
+++ lua/user/plugins/multi-cursors.lua
@@ -1,4 +1,5 @@
 local M = {
+  -- other option: terryma/vim-multiple-cursors
   "smoka7/multicursors.nvim",
   event = "VeryLazy",
   dependencies = {
@@ -24,6 +25,14 @@ function M.config()
       },
     },
   }
+
+  vim.api.nvim_set_hl(
+    0,
+    "MultiCursor",
+    { fg = "none", bg = vim.api.nvim_get_hl(0, { name = "Visual", link = false }).bg, bold = true }
+  )
+  vim.api.nvim_set_hl(0, "MultiCursorMain", { fg = "#ffffff", bg = "none", bold = true })
+  vim.api.nvim_set_hl(0, "MultiCursorVisual", { fg = "#000000", bg = "none", underline = true })
 end
 
 return M
