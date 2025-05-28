b8c0cb3d7a6aa8b6e4841ac91112c6fd8ad30f4b
Enrique Mendoza <luis.e.1998@hotmail.com>
2025-05-27 19:00:50 -0600

    update(options): disable health check for providers

--- lua/user/core/options.lua
+++ lua/user/core/options.lua
@@ -47,3 +47,9 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.netrw_banner = 0
 vim.g.netrw_mouse = 2
+
+-- disable health checks for these providers.
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
