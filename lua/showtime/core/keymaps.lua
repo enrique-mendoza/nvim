5f65f46e5ed133c24322b36525606239ee29bd55
Enrique Mendoza <luis.e.1998@hotmail.com>
2025-05-29 10:42:58 -0600

    update: keymaps

--- lua/showtime/core/keymaps.lua
+++ lua/showtime/core/keymaps.lua
@@ -2,82 +2,83 @@ local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 keymap("n", "<Space>", "", opts)
-keymap("n", "<C-i>", "<C-i>", opts)
+-- keymap("n", "<C-i>", "<C-i>", opts)
 
 keymap("n", "<Esc>", ":nohlsearch<CR>", opts)
 
+-- Remap for dealing with word wrap and adding jumps to the jumplist.
+keymap("n", "j", [[(v:count > 1 ? 'm`' . v:count : 'g') . 'j']], { expr = true })
+keymap("n", "k", [[(v:count > 1 ? 'm`' . v:count : 'g') . 'k']], { expr = true })
+
+-- Make U opposite to u.
+keymap("n", "U", "<C-r>", opts)
+
+-- Switch between windows.
+-- I can also use Ctrl key using vim-tmux-navigator plugin.
 keymap("n", "<m-h>", "<C-w>h", opts)
 keymap("n", "<m-j>", "<C-w>j", opts)
 keymap("n", "<m-k>", "<C-w>k", opts)
 keymap("n", "<m-l>", "<C-w>l", opts)
 keymap("n", "<m-tab>", "<c-6>", opts)
 
+-- Resize windows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
-keymap("n", "n", "nzz", opts)
-keymap("n", "N", "Nzz", opts)
-keymap("n", "*", "*zz", opts)
-keymap("n", "#", "#zz", opts)
-keymap("n", "g*", "g*zz", opts)
-keymap("n", "g#", "g#zz", opts)
+-- Keeping the cursor centered.
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
+keymap("n", "n", "nzzzv", opts)
+keymap("n", "N", "Nzzzv", opts)
+keymap("n", "*", "*zzzv", opts)
+keymap("n", "#", "#zzzv", opts)
+keymap("n", "g*", "g*zzzv", opts)
+keymap("n", "g#", "g#zzzv", opts)
 
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
-
+-- Move a chunk of text and it will automatically indent.
 keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
 keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
 
-keymap("n", "d", '"_d', opts)
-keymap("v", "d", '"_d', opts)
+-- Indent while remaining in visual mode.
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
 
-keymap("x", "p", [["_dP]], opts)
+-- Search within visual selection - this is magic
+keymap("x", "/", "<Esc>/\\%V")
 
-keymap({ "n", "v" }, "<leader>y", '"+y"')
-
-keymap("n", "<RightMouse>", "<cmd>:popup mousemenu<CR>")
-keymap("n", "<Tab>", "<cmd>:popup mousemenu<CR>")
-
+-- Improving ^ and $ keymaps.
 keymap({ "n", "o", "x" }, "<s-h>", "^", opts)
 keymap({ "n", "o", "x" }, "<s-l>", "g_", opts)
 
-keymap({ "n", "x" }, "j", "gj", opts)
-keymap({ "n", "x" }, "k", "gk", opts)
-keymap("n", "<leader>w", ":lua vim.wo.wrap = not vim.wo.wrap<CR>", opts)
-
--- keymap("n", "<s-tab>", "<cmd>tabnew %<cr>", opts)
--- keymap({ "n" }, "<s-h>", "<cmd>tabp<cr>", opts)
--- keymap({ "n" }, "<s-l>", "<cmd>tabn<cr>", opts)
-
+-- Exit Terminal input and bring you back to Normal mode in that terminal buffer.
 keymap("t", "<C-;>", "<C-\\><C-n>", opts)
 
--- put your cursor to same position while yanking
-keymap("n", "p", function()
-  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-  vim.cmd "put"
-  vim.api.nvim_win_set_cursor(0, { row + 1, col })
-end)
+-- Escape and save changes.
+keymap({ "s", "i", "n", "v" }, "<C-s>", "<esc>:w<cr>", opts)
+keymap({ "s", "i", "n", "v" }, "<C-S-s>", "<esc>:wa<cr>", opts)
 
--- search within visual selection - this is magic
-keymap("x", "/", "<Esc>/\\%V")
+-- Quickly go to the end of the line while in insert mode.
+keymap({ "i", "c" }, "<C-l>", "<C-o>A", opts)
 
--- duplicate line and comment the first line
+-- Duplicate line and comment the first line
 keymap("n", "ycc", "yygccp", { remap = true })
 
--- make j and k movements with multiple save to jumplist
--- for example jump 12 lines down with "12j" and go back to where you ran that motion with C-o
-keymap("n", "j", function()
-  if vim.v.count > 0 then
-    return "m'" .. vim.v.count .. "j"
-  end
-  return "j"
-end, { expr = true })
+-- Put your cursor to same position while yanking
+-- keymap("n", "p", function()
+--   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+--   vim.cmd "put"
+--   vim.api.nvim_win_set_cursor(0, { row + 1, col })
+-- end)
 
-keymap("n", "k", function()
-  if vim.v.count > 0 then
-    return "m'" .. vim.v.count .. "k"
-  end
-  return "k"
-end, { expr = true })
+-- Improving pasting, copying and deleting keymaps.
+-- keymap("x", "<leader>p", [["_dP]])
+
+-- keymap({ "n", "v" }, "<leader>y", [["+y]])
+-- keymap("n", "<leader>Y", [["+Y]])
+
+-- keymap({ "n", "v" }, "<leader>d", '"_d')
+
+-- I don't know why I have this.
+keymap("n", "<RightMouse>", "<cmd>:popup mousemenu<CR>")
