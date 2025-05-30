3164a06426a20b2c346ef8dd882d99693799348f
Enrique Mendoza <luis.e.1998@hotmail.com>
2025-05-30 17:06:36 -0600

    update(grug-far): quit with esc

--- lua/showtime/plugins/grug-far.lua
+++ lua/showtime/plugins/grug-far.lua
@@ -5,6 +5,14 @@ local M = {
 
 function M.config()
   require("grug-far").setup {}
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "grug-far",
+    callback = function()
+      -- Map <Esc> to quit after ensuring we're in normal mode
+      vim.keymap.set({ "i", "n" }, "<Esc>", "<Cmd>stopinsert | bd!<CR>", { buffer = true })
+    end,
+  })
 end
 
 return M
