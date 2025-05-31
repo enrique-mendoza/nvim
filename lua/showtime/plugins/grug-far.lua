153510bf48e0294f83439317d655452a5b5314d7
Enrique Mendoza <luis.e.1998@hotmail.com>
2025-05-30 21:52:20 -0600

    update(grug-far): exit normal mode

--- lua/showtime/plugins/grug-far.lua
+++ lua/showtime/plugins/grug-far.lua
@@ -10,7 +10,7 @@ function M.config()
     pattern = "grug-far",
     callback = function()
       -- Map <Esc> to quit after ensuring we're in normal mode
-      vim.keymap.set({ "i", "n" }, "<Esc>", "<Cmd>stopinsert | bd!<CR>", { buffer = true })
+      vim.keymap.set("n", "<Esc>", "<Cmd>stopinsert | bd!<CR>", { buffer = true })
     end,
   })
 end
