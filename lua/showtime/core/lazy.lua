a468eaa258ee31ba2996abffe2aa8ac337142486
Enrique Mendoza <luis.e.1998@hotmail.com>
2025-06-01 11:12:34 -0600

    update(lazy): add wk to config

--- lua/showtime/core/lazy.lua
+++ lua/showtime/core/lazy.lua
@@ -26,3 +26,46 @@ require("lazy").setup {
     notify = false,
   },
 }
+
+require("which-key").add {
+  {
+    "<leader>pc",
+    "<cmd>Lazy clean<cr>",
+    desc = "Clean",
+  },
+  {
+    "<leader>pd",
+    "<cmd>Lazy debug<cr>",
+    desc = "Debug",
+  },
+  {
+    "<leader>pi",
+    "<cmd>Lazy install<cr>",
+    desc = "Install",
+  },
+  {
+    "<leader>pl",
+    "<cmd>Lazy log<cr>",
+    desc = "Log",
+  },
+  {
+    "<leader>pp",
+    "<cmd>Lazy profile<cr>",
+    desc = "Profile",
+  },
+  {
+    "<leader>ps",
+    "<cmd>Lazy sync<cr>",
+    desc = "Sync",
+  },
+  {
+    "<leader>pS",
+    "<cmd>Lazy clear<cr>",
+    desc = "Status",
+  },
+  {
+    "<leader>pu",
+    "<cmd>Lazy update<cr>",
+    desc = "Update",
+  },
+}
--- lua/showtime/plugins/which-key.lua
+++ lua/showtime/plugins/which-key.lua
@@ -160,46 +160,6 @@ function M.config()
         color = "purple",
       },
     },
-    {
-      "<leader>pc",
-      "<cmd>Lazy clean<cr>",
-      desc = "Clean",
-    },
-    {
-      "<leader>pd",
-      "<cmd>Lazy debug<cr>",
-      desc = "Debug",
-    },
-    {
-      "<leader>pi",
-      "<cmd>Lazy install<cr>",
-      desc = "Install",
-    },
-    {
-      "<leader>pl",
-      "<cmd>Lazy log<cr>",
-      desc = "Log",
-    },
-    {
-      "<leader>pp",
-      "<cmd>Lazy profile<cr>",
-      desc = "Profile",
-    },
-    {
-      "<leader>ps",
-      "<cmd>Lazy sync<cr>",
-      desc = "Sync",
-    },
-    {
-      "<leader>pS",
-      "<cmd>Lazy clear<cr>",
-      desc = "Status",
-    },
-    {
-      "<leader>pu",
-      "<cmd>Lazy update<cr>",
-      desc = "Update",
-    },
     -- Search/Replace
     {
       "<leader>r",
