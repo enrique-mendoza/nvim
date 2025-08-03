c3be6c9dfff2dfef90d4fb5252b00640426e6f43
Enrique Mendoza <luis.e.1998@hotmail.com>
2025-08-03 14:19:31 -0600

    update(auto-session): borders

--- lua/user/plugins/auto-session.lua
+++ lua/user/plugins/auto-session.lua
@@ -49,7 +49,7 @@ function M.config()
     session_lens = {
       -- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
       load_on_setup = true,
-      theme_conf = { border = true },
+      picker_opts = { border = true },
       previewer = false,
     },
   }
