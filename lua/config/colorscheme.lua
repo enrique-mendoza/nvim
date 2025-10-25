1448981049d301cea41fabd604679c5fd6a8472a
Enrique Mendoza <luis.e.1998@hotmail.com>
2025-10-24 20:06:04 -0600

    feat(themes): tokyonight

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,6 +1,7 @@
 -- Colorscheme configuration
 local colorscheme = "catppuccin"
 -- local colorscheme = "kanagawa-wave" -- wave, dragon, lotus
+-- local colorscheme = "tokyonight-night" -- night, storm, day, moon
 
 -- Function to safely set colorscheme
 local function set_colorscheme(name)
