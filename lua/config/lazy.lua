c235125b4c423d3dcb273e5990922a5e33098bbf
Enrique Mendoza <luis.e.1998@hotmail.com>
2026-01-23 23:13:04 -0600

    feat(lang): tailwind and yaml

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -33,8 +33,10 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.toml" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
     -- import/override with your plugins
     { import = "plugins" },
   },
